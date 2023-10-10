5578503b7cc7294979a1728a786774952b94aecb
Tomy2931 <tomas.cejas@hotmail.com>
2023-10-10 03:41:45 -0300

    Pantalla numerica

--- src/Pages/Home/Screens/MainTitle/Spanish/contactSpanish/phone.jsx
+++ src/Pages/Home/Screens/MainTitle/Spanish/contactSpanish/phone.jsx
@@ -5,7 +5,9 @@ function phoneBox() {
     return (
            <div className='phoneBox'>
                 <div className='phoneHandle'></div>
-                
+                <div className='numberScreen'>
+                    <p className='numberScreenCont'>5125</p>
+                </div>
                 <div className='phoneButtonBox'></div>
            </div>
          
